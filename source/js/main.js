be5ca09cd2b4014ca91b4650ce7de4ade8dda762
Sergulova Luiza <lyisas@mail.ru>
2021-11-08 04:09:05 +0300

    first check

--- source/js/main.js
+++ source/js/main.js
@@ -1,123 +1,129 @@
 "use strict";
 
 (function () {
-  const fitnessRoom = document.querySelector(".fitness-room__video");
-  const fitnessRoomPlug = fitnessRoom.querySelector(".fitness-room__video > div");
-  const fitnessRoomVideo = fitnessRoom.querySelector(".fitness-room__video > video");
-  const fitnessRoomVideoBtn = fitnessRoom.querySelector(".fitness-room__video-btn");
+  if (document.querySelector(".fitness-room__video") !== null) {
+    const fitnessRoom = document.querySelector(".fitness-room__video");
+    const fitnessRoomPlug = fitnessRoom.querySelector(".fitness-room__video > div");
+    const fitnessRoomVideo = fitnessRoom.querySelector(".fitness-room__video > video");
+    const fitnessRoomVideoBtn = fitnessRoom.querySelector(".fitness-room__video-btn");
 
-  fitnessRoom.classList.add("fitness-room__video--disabled");
+    fitnessRoom.classList.add("fitness-room__video--disabled");
 
-  if (fitnessRoom.classList.contains("fitness-room__video--disabled")) {
-    fitnessRoomVideo.removeAttribute("controls");
+    if (fitnessRoom.classList.contains("fitness-room__video--disabled")) {
+      fitnessRoomVideo.removeAttribute("controls");
+    }
+
+    const showVideo = function () {
+      fitnessRoomVideoBtn.classList.add("hidden");
+      fitnessRoomPlug.classList.add("hidden");
+
+      fitnessRoomVideo.setAttribute("controls", "true");
+      fitnessRoomVideo.removeAttribute("poster");
+      fitnessRoomVideo.play();
+    };
+
+    fitnessRoomVideoBtn.addEventListener("click", function() {
+      showVideo();
+    });
+    }
+})();
+
+"use strict";
+
+(function () {
+  if (document.querySelector(".trainers-slider") !== null && document.querySelector(".reviews-slider") !== null) {
+    const mySwiper = new Swiper(".trainers-slider", {
+      loop: true,
+      speed: 900,
+      navigation: {
+        nextEl: ".trainers__button--next",
+        prevEl: ".trainers__button--prev",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          slidesPerGroup: 1
+        },
+        768: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween: 30
+        },
+        1200: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          spaceBetween: 40
+        }
+      }
+    });
+
+    const swiper = new Swiper(".reviews-slider", {
+      loop: true,
+      speed: 900,
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".reviews__button--next",
+        prevEl: ".reviews__button--prev",
+      }
+    });
   }
-
-  const showVideo = function () {
-    fitnessRoomVideoBtn.classList.add("hidden");
-    fitnessRoomPlug.classList.add("hidden");
-
-    fitnessRoomVideo.setAttribute("controls", "true");
-    fitnessRoomVideo.removeAttribute("poster");
-    fitnessRoomVideo.play();
-  };
-
-  fitnessRoomVideoBtn.addEventListener("click", function() {
-    showVideo();
-  });
 })();
 
 "use strict";
 
 (function () {
-  const mySwiper = new Swiper(".trainers-slider", {
-    loop: true,
-    speed: 900,
-    navigation: {
-      nextEl: ".trainers-slider__button--next",
-      prevEl: ".trainers-slider__button--prev",
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        slidesPerGroup: 1
-      },
-      768: {
-        slidesPerView: 2,
-        slidesPerGroup: 2,
-        spaceBetween: 30
-      },
-      1200: {
-        slidesPerView: 4,
-        slidesPerGroup: 4,
-        spaceBetween: 40
+  if (document.querySelector(".feedback-form") !== null) {
+    const form = document.querySelector(".feedback-form");
+    const formFileds = form.elements;
+    const submitBtn = form.querySelector(".feedback-form__btn");
+    const telInput = form.querySelector("input[name='tel']");
+
+    const changeHandler = function () {
+      localStorage.setItem(this.name, this.value);
+    };
+
+    const attachEvents = function () {
+      for (let i = 0; i < formFileds.length; i++) {
+        formFileds[i].addEventListener("change", changeHandler);
       }
-    }
-  });
+    };
 
-  const swiper = new Swiper(".reviews-slider", {
-    loop: true,
-    speed: 900,
-    slidesPerView: 1,
-    slidesPerGroup: 1,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".reviews-slider__button--next",
-      prevEl: ".reviews-slider__button--prev",
-    }
-  });
-})();
-
-"use strict";
-
-(function () {
-  const form = document.querySelector(".feedback-form");
-  const formFileds = form.elements;
-  const submitBtn = form.querySelector(".feedback-form__btn");
-  const telInput = form.querySelector("input[name='tel']");
-
-  const changeHandler = function () {
-    localStorage.setItem(this.name, this.value);
-  };
-
-  const attachEvents = function () {
-    for (let i = 0; i < formFileds.length; i++) {
-      formFileds[i].addEventListener("change", changeHandler);
-    }
-  };
-
-  const checkStorage = function () {
-    for (let i = 0; i < formFileds.length; i++) {
-      if (formFileds[i].type !== "submit") {
-        formFileds[i].value = localStorage.getItem(formFileds[i].name);
+    const checkStorage = function () {
+      for (let i = 0; i < formFileds.length; i++) {
+        if (formFileds[i].type !== "submit") {
+          formFileds[i].value = localStorage.getItem(formFileds[i].name);
+        }
       }
-    }
 
-    attachEvents();
-  };
+      attachEvents();
+    };
 
-  checkStorage();
+    checkStorage();
 
-  const clearStorage = function () {
-    localStorage.clear();
-  };
+    const clearStorage = function () {
+      localStorage.clear();
+    };
 
-  const checkEmptyField = function () {
-    let valueLength = Number (telInput.value.length);
+    const checkEmptyField = function () {
+      let valueLength = Number (telInput.value.length);
 
-    if(valueLength < 18) {
-      telInput.setCustomValidity("Номер телефона должен состоять из 11 цифр");
-    } else {
-      telInput.setCustomValidity("");
-    }
-  };
+      if(valueLength < 18) {
+        telInput.setCustomValidity("Номер телефона должен состоять из 11 цифр");
+      } else {
+        telInput.setCustomValidity("");
+      }
+    };
 
-  form.addEventListener("input", function() {
-    checkEmptyField();
-  });
+    form.addEventListener("input", function() {
+      checkEmptyField();
+    });
 
-  submitBtn.addEventListener("click", function() {
-    clearStorage();
-  });
+    submitBtn.addEventListener("click", function() {
+      clearStorage();
+    });
+  }
 })();
 
 "use strict";
@@ -194,5 +200,116 @@
     phoneInput.addEventListener("keydown", onPhoneKeyDown);
     phoneInput.addEventListener("input", onPhoneInput, false);
     phoneInput.addEventListener("paste", onPhonePaste, false);
+  }
+})();
+
+"use strict";
+
+(function () {
+  if (document.querySelector(".schedule") !== null) {
+
+    const schedule = document.querySelector(".schedule");
+    const scheduleLesson = schedule.querySelectorAll(".schedule__lesson");
+    const scheduleWeekday = schedule.querySelectorAll(".schedule__weekday");
+    const scheduleTime = schedule.querySelectorAll(".schedule__time");
+    const scheduleBtn = schedule.querySelector(".schedule__btn");
+    const scheduleWeekdays = schedule.querySelector(".schedule__weekdays-wrapper");
+    const scheduleDays = schedule.querySelectorAll(".schedule__weekday");
+
+    const colorSwitching = function (evt) {
+        let target = evt.target;
+
+        scheduleLesson.forEach(function(elem) {
+          if(elem.classList.contains("schedule--active")) {
+            if (!target.classList.contains("schedule--active")) {
+              elem.classList.add("schedule--click");
+            }
+          }
+        });
+
+        if (!target.classList.contains("schedule--click")) {
+            target.classList.add("schedule--active");
+          }
+
+        if (target.classList.contains("schedule--click")) {
+          target.classList.remove("schedule--click")
+          target.classList.remove("schedule--active")
+        }
+      }
+
+      const addDatasetColor = function (evt) {
+        let target = evt.target;
+
+        if (target.classList.contains("schedule--active")) {
+          scheduleTime.forEach(function(elem) {
+            elem.classList.remove("schedule--weekday-time");
+
+            if (target.dataset.time == elem.dataset.time) {
+              elem.classList.add("schedule--weekday-time");
+            }
+          });
+
+          scheduleWeekday.forEach(function(elem) {
+            elem.classList.remove("schedule--weekday-time");
+
+            if (target.dataset.weekday == elem.dataset.weekday) {
+              elem.classList.add("schedule--weekday-time");
+            }
+          });
+        }
+      }
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      const openWeekdays = function () {
+        if (scheduleWeekdays.classList.contains("schedule__weekdays--closed")) {
+          scheduleWeekdays.classList.remove("schedule__weekdays--closed");
+          scheduleWeekdays.classList.add("schedule__weekday--active");
+        } else {
+          scheduleWeekdays.classList.add("schedule__weekdays--closed");
+          scheduleWeekdays.classList.remove("schedule__weekday--active");
+        }
+      }
+
+      const addActiveWeekDays = function (evt) {
+        let target = evt.target;
+
+        scheduleDays.forEach(function(item) {
+          item.classList.remove("schedule__weekday--active");
+        });
+
+        target.classList.add("schedule__weekday--active");
+        scheduleWeekdays.classList.add("schedule__weekdays--closed");
+      }
+
+      const addLeson = function (evt) {
+        let target = evt.target;
+
+        scheduleLesson.forEach(function(elem) {
+          elem.style.display = "none";
+
+          if(target.dataset.weekday == elem.dataset.weekday) {
+            elem.style.display = "block";
+          }
+        });
+      }
+
+      scheduleBtn.addEventListener("click", function() {
+        openWeekdays();
+      });
+
+      scheduleDays.forEach(function(item) {
+        item.addEventListener("click", function(evt) {
+          addActiveWeekDays(evt);
+          addLeson(evt);
+        });
+      });
+    }
+
+    scheduleLesson.forEach(function(elem) {
+      elem.addEventListener("click", function(evt) {
+        colorSwitching(evt);
+        addDatasetColor(evt);
+      });
+    });
   }
 })();
